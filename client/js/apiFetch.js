77ae261a3569230169893789a0d0d3699cbdfa96
Logan Zechella <jloganzechella@gmail.com>
2022-11-05 20:57:42 -0400

    File tree changes

---
+++ client/js/apiFetch.js
@@ -0,0 +1,70 @@
+
+import { scannedText } from './addAllergens.js';
+import { signupUserInfo } from './newUser.js.js.js';
+
+// API Data
+export let apiOutput;
+
+export async function getAPI() {
+    const urlPtOne = 'https://api.edamam.com/api/food-database/v2/parser?app_id=ca2296e3&app_key=a2e456b4aee5d50b688e3b76c657f79f&upc=';
+    let fetchUrl = new URL(urlPtOne + scannedText);
+    const resjson = await fetch(fetchUrl).then(function (response) {
+        // The API call was successful
+        return response.json();
+    }).then(function (data) {
+        // This is the JSON from response
+        let name = data.hints[0].food.label;
+        let ingredients = data.hints[0].food.foodContentsLabel;
+        apiOutput = {
+            name: name,
+            ingredients: ingredients
+        }
+    })
+    .catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    })
+    return apiOutput; 
+}
+
+
+// DB
+
+
+export async function insertDB() {
+    let newUser = JSON.stringify({
+        user_name: 'test',
+        user_email: 'test',
+        user_password: 'test',
+        user_allergens: 'test',
+    });
+
+    const send = await fetch('https://data.mongodb-api.com/app/data-zlrzk/endpoint/data/v1/action/insertOne', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            "dataSource": "allergenScanner",
+            "database": "allergenscanner",
+            "collection": "users",
+            "document": {
+                "user_name": newUser.user_name,
+                "user_email": newUser.user_email,
+                "user-password": newUser.user_password,
+                "user_allergens": newUser.user_allergens
+            },
+            "body": newUser,
+        }
+        }).then(response => {
+        //handle response            
+        console.log(response);
+        })
+        .then(data => {
+            //handle data
+            console.log(data);
+        })
+        .catch(error => {
+            //handle error
+        });
+}
+
---
+++ client/js/index.js
@@ -0,0 +1,142 @@
+import insertDB from './newUser.js.js.js';
+
+const menuItemLoginBtn = document.getElementById("menu-login");
+var burger = document.querySelector('.burger-container');
+var header = document.querySelector('.menu-container');
+const menuContainer = document.querySelector('.menu-container');
+const landingHeader = document.getElementById("landing-header");
+const addAllergenWindow = document.getElementById("add-allergen-window");
+const loginWindow = document.getElementById("login-window");
+const hero = document.getElementById("hero");
+const loginBtn = document.getElementById("header-login-btn");
+const stopScanBtn = document.getElementById("stop-scan-btn");
+const menuItemScanBtn = document.getElementById("menu-start");
+const signupWindow = document.getElementById("signup-window");
+const menuItemSignupBtn = document.getElementById("menu-signup");
+const signupBtnSubmit = document.getElementById("signup-new-btn");
+
+// Burger Toggle 
+function burgerToggle() {
+    addAllergenWindow.style.display = "flex";
+    hero.style.display = "none";
+    !loginBtn.style.display;
+}
+
+burger.addEventListener('click', function () {
+    header.classList.toggle('menu-opened');
+});
+
+// Menu Login 
+function menuLoginToggle() {
+    menuContainer.classList.toggle('menu-opened');
+    if (!menuContainer.classList.contains('menu-opened')) {
+	    loginWindow.style.display = "flex";
+	    hero.style.display = "none";
+	    loginBtn.style.display = "none";
+        addAllergenWindow.style.display = "none";
+        signupWindow.style.display = "none";
+    } else {
+        setTimeout(menuLoginToggle, 800);
+    }
+};
+
+// Menu Scan
+function menuScanToggle() {
+    menuContainer.classList.toggle('menu-opened');
+    if (!menuContainer.classList.contains('menu-opened')) {
+        addAllergenWindow.style.display = "flex";
+        hero.style.display = "none";
+        loginBtn.style.display = "none";
+        loginWindow.style.display = "none";
+        signupWindow.style.display = "none";
+        hamburgerAnimate();
+    } else {
+        setTimeout(burgerToggle, 800);
+    }
+};
+
+// Menu Signup
+function menuSignupToggle() {
+    menuContainer.classList.toggle('menu-opened');
+    if (!menuContainer.classList.contains('menu-opened')) {
+        signupWindow.style.display = "flex";
+        hero.style.display = "none";
+        loginBtn.style.display = "none";
+        loginWindow.style.display = "none";
+        hamburgerAnimate();
+    } else {
+        setTimeout(burgerToggle, 800);
+    }
+};
+
+// Menu Item Event Listeners
+menuItemLoginBtn.addEventListener("click", menuLoginToggle);
+menuItemScanBtn.addEventListener("click", menuScanToggle);
+menuItemSignupBtn.addEventListener("click", menuSignupToggle);
+
+// Open Add Allergen Window
+function startNow() {
+    addAllergenWindow.style.display = "flex";
+    hero.style.display = "none";
+    loginBtn.style.display = "none";
+    landingHeader.style.padding = "0.5em 0 0.5em 0";
+};
+
+// Open Login Window
+function openLogin() {
+    loginWindow.style.display = "flex";
+    hero.style.display = "none";
+    loginBtn.style.display = "none";
+    landingHeader.style.padding = "1em 1em 1em 0";
+}
+
+loginBtn.addEventListener("click", openLogin);
+
+// Close Login Window
+function closeLoginWindow() {
+    loginWindow.style.display = "none";
+    addAllergenWindow.style.display = "none";
+    hero.style.display = "flex";
+    landingHeader.style.display = "flex";
+    loginBtn.style.display = "block";
+};
+
+// Close Add Allergen Window
+function closeAddAllergen() {
+    addAllergenWindow.style.display = "none";
+    hero.style.display = "flex";
+    loginBtn.style.display = "block";
+    landingHeader.style.display = "flex";
+};
+
+// Close Signup Window
+function closeSignup() {
+    signupWindow.style.display = "none";
+    hero.style.display = "flex";
+    loginBtn.style.display = "block";
+    landingHeader.style.display = "flex";
+};
+
+// Close Button Event Listeners
+let loginCloseBtn = document.getElementById('login-close-btn');
+let startNowbtn = document.getElementById('start-btn');
+let addAllergenCloseBtn = document.getElementById('add-allergen-close-btn');
+let signupCloseBtn = document.getElementById('signup-close-btn');
+
+loginCloseBtn.addEventListener('click', closeLoginWindow);
+addAllergenCloseBtn.addEventListener('click', closeAddAllergen);
+startNowbtn.addEventListener('click', startNow);
+signupCloseBtn.addEventListener('click', closeSignup);
+
+// Hamburger Animations
+function hamburgerAnimate() {
+    let menuStart = document.querySelector('.menu-start');
+    menuStart.addEventListener('click', function () {
+        document.querySelector('.menu-container').classList.toggle('menu-opened');
+        if (!menuContainer.classList.contains('menu-opened')) {
+            hamburgerAnimate();
+        } else {
+            setTimeout(burgerToggle, 800);
+        }
+    });
+};
